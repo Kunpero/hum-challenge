82dc7847b82a594aa855f0c44e680b8779f1ad5d
kunpero <kunperooo@gmail.com>
2019-11-29 10:53:20 +0300

    refactoring

--- src/main/java/rs/kunpero/humchallenge/factory/QuestionnaireFactory.java
+++ src/main/java/rs/kunpero/humchallenge/factory/QuestionnaireFactory.java
@@ -1,15 +1,11 @@
 package rs.kunpero.humchallenge.factory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import rs.kunpero.humchallenge.api.QuestionnaireManager;
 import rs.kunpero.humchallenge.api.QuestionnaireManagerImpl;
 import rs.kunpero.humchallenge.integration.ExternalService;
 import rs.kunpero.humchallenge.service.QuestionnaireService;
 
 public class QuestionnaireFactory {
-    private static final Logger log = LoggerFactory.getLogger(QuestionnaireFactory.class);
-
     public static QuestionnaireManager getManager(ExternalService externalService) {
         QuestionnaireService service = new QuestionnaireService(externalService);
         return new QuestionnaireManagerImpl(service);
